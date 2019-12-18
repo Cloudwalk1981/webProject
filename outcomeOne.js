922f53ec3d4b4d6466316a6f521dc90c5682c5d8
CN 4 <administrator@CN-4.local>
2019-12-18 13:44:42 +0000

    Final Site

--- outcomeOne.js
+++ outcomeOne.js
@@ -1,4 +1,10 @@
 let myParagraph = document.getElementById("myP")
+let myParagraph2 = document.getElementById("myP2")
+let myParagraph3 = document.getElementById("myP3")
+let myParagraph4 = document.getElementById("myP4")
+let myParagraph5 = document.getElementById("myP5")
+let myParagraph6 = document.getElementById("myP6")
+let myParagraph7 = document.getElementById("myP7")
 
 let button1 = document.getElementById("1.1.1")
 let button2 = document.getElementById("1.1.2")
@@ -25,61 +31,190 @@ let button20 = document.getElementById("2.1.7")
 
 button1.addEventListener('click', () => {
     myParagraph.textContent = "IT has a critical role in the modern business environment. All busy dealing are, at a base level, about exchanges of information. Whether that be something as complicated and evaluating the taxes of a huge corporation, or if it is something as simple as telling the cashier how you would like you coffee to be prepared. Well, IT is literally Information Technology. The technology to access, transfer and store huge quantities of data. This technology allows businesses and individuals to manipulate, collate, share, hide or reveal simply huge amounts of data that would have been nothing short of miraculous before the modern era."
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
+
 })
 button2.addEventListener('click', () => {
-    myParagraph.textContent = "There are a multitude of different options when it comes to design and development of any digital project. These tools can be used in a hands on manner to directly code the new project, or they can be of indirect assistance and help you organise your time more efficiently, or to coordinate more seamlessly with a team, if you are working on a group project. \n The basics things you would likely need are; an integrated development environment such as Visual Studio Code and the skills to use this program correctly so that you can successfully create whatever digital project that you originally designed."
+    myParagraph.textContent = "There are a multitude of different options when it comes to design and development of any digital project. These tools can be used in a hands on manner to directly code the new project, or they can be of indirect assistance and help you organise your time more efficiently, or to coordinate more seamlessly with a team, if you are working on a group project."
+    myParagraph2.textContent = "The basics things you would likely need are; an integrated development environment such as Visual Studio Code and the skills to use this program correctly so that you can successfully create whatever digital project that you originally designed."
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
+
 })
 button3.addEventListener('click', () => {
     myParagraph.textContent = "Before any code is written for the website, I intend to go through a thorough design phase so that I know what tasks are required, so that I can optimise my time on the project. Trello, and the Kanban method will be particularly useful for this."
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
+
 })
 button4.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = "There are a number of things that could be mishandled and create significant risk to your ability to achieve your objective. They include, but are not limited to:"
+    myParagraph2.textContent = "Bad planning"
+    myParagraph3.textContent = "Setting unrealistic goals"
+    myParagraph4.textContent = "Inefficient use of time"
+    myParagraph5.textContent = "Lack of project coordination"
+    myParagraph6.textContent = "Getting bogged down with problem solving"
+    myParagraph7.textContent = "Bad marketing / search priority"
+
 })
 button5.addEventListener('click', () => {
     myParagraph.textContent = "The only way that you could know which technology and tools are appropriate and the best fit for any given task is to carefully research all of your options, being careful to cross-reference hardware, software and operating systems that are suited to your plan, and making sure that they do indeed all run and integrate properly."
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
+
 })
 button6.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = ""
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
 })
 button7.addEventListener('click', () => {
     myParagraph.textContent = "The General Data Protection Regulation prevents people taking your data and selling it, or otherwise passing it on to other users."
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
+
 })
 button8.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = ""
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
+
 })
 button9.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = ""
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
 })
 button10.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = ""
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
 })
 button11.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = ""
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
 })
 button12.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = ""
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
 })
 button13.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = ""
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
 })
 button14.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = ""
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
 })
 button15.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = ""
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
 })
 button16.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = ""
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
 })
 button17.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = ""
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
 })
 button18.addEventListener('click', () => {
     myParagraph.textContent = "Copyright is a safeguard that protects intellectual properties so that you cannot simply plagiarise someone else’s work and claim it as your own, specifically in regard to monetisation, but also in general. This means that if you have to use images that you have not created in your project then you should, as part of your own due diligence, ensure that the image is free to use and does not violate any copyright laws."
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
+
 })
 button19.addEventListener('click', () => {
     myParagraph.textContent = "To test the ease of access of uses with disabilities you can use a Google Chrome extension called “Funkify” to simulate other users requirements and then, using this, see if you can improve the website for other people."
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
+
 })
 button20.addEventListener('click', () => {
-    myParagraph.textContent = "Hello"
+    myParagraph.textContent = ""
+    myParagraph2.textContent = ""
+    myParagraph3.textContent = ""
+    myParagraph4.textContent = ""
+    myParagraph5.textContent = ""
+    myParagraph6.textContent = ""
+    myParagraph7.textContent = ""
 })
