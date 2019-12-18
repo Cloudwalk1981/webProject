01794807c34658879d0cc2153b3afd48164dee83
CN 4 <administrator@CN-4.local>
2019-12-18 11:50:15 +0000

    Nearly finished

---
+++ outcomeOne.js
@@ -0,0 +1,85 @@
+let myParagraph = document.getElementById("myP")
+
+let button1 = document.getElementById("1.1.1")
+let button2 = document.getElementById("1.1.2")
+let button3 = document.getElementById("1.1.3")
+let button4 = document.getElementById("1.1.4")
+let button5 = document.getElementById("1.1.5")
+let button6 = document.getElementById("1.1.6")
+let button7 = document.getElementById("1.1.7")
+let button8 = document.getElementById("1.2.1")
+let button9 = document.getElementById("1.2.2")
+let button10 = document.getElementById("1.2.3")
+let button11 = document.getElementById("1.2.4")
+let button12 = document.getElementById("1.2.5")
+let button13 = document.getElementById("1.3.1")
+let button14 = document.getElementById("1.3.2")
+let button15 = document.getElementById("1.3.3")
+let button16 = document.getElementById("1.3.4")
+let button17 = document.getElementById("2.1.1")
+let button18 = document.getElementById("2.1.5")
+let button19 = document.getElementById("2.1.6")
+let button20 = document.getElementById("2.1.7")
+
+
+
+button1.addEventListener('click', () => {
+    myParagraph.textContent = "IT has a critical role in the modern business environment. All busy dealing are, at a base level, about exchanges of information. Whether that be something as complicated and evaluating the taxes of a huge corporation, or if it is something as simple as telling the cashier how you would like you coffee to be prepared. Well, IT is literally Information Technology. The technology to access, transfer and store huge quantities of data. This technology allows businesses and individuals to manipulate, collate, share, hide or reveal simply huge amounts of data that would have been nothing short of miraculous before the modern era."
+})
+button2.addEventListener('click', () => {
+    myParagraph.textContent = "There are a multitude of different options when it comes to design and development of any digital project. These tools can be used in a hands on manner to directly code the new project, or they can be of indirect assistance and help you organise your time more efficiently, or to coordinate more seamlessly with a team, if you are working on a group project. \n The basics things you would likely need are; an integrated development environment such as Visual Studio Code and the skills to use this program correctly so that you can successfully create whatever digital project that you originally designed."
+})
+button3.addEventListener('click', () => {
+    myParagraph.textContent = "Before any code is written for the website, I intend to go through a thorough design phase so that I know what tasks are required, so that I can optimise my time on the project. Trello, and the Kanban method will be particularly useful for this."
+})
+button4.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
+button5.addEventListener('click', () => {
+    myParagraph.textContent = "The only way that you could know which technology and tools are appropriate and the best fit for any given task is to carefully research all of your options, being careful to cross-reference hardware, software and operating systems that are suited to your plan, and making sure that they do indeed all run and integrate properly."
+})
+button6.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
+button7.addEventListener('click', () => {
+    myParagraph.textContent = "The General Data Protection Regulation prevents people taking your data and selling it, or otherwise passing it on to other users."
+})
+button8.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
+button9.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
+button10.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
+button11.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
+button12.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
+button13.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
+button14.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
+button15.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
+button16.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
+button17.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
+button18.addEventListener('click', () => {
+    myParagraph.textContent = "Copyright is a safeguard that protects intellectual properties so that you cannot simply plagiarise someone else’s work and claim it as your own, specifically in regard to monetisation, but also in general. This means that if you have to use images that you have not created in your project then you should, as part of your own due diligence, ensure that the image is free to use and does not violate any copyright laws."
+})
+button19.addEventListener('click', () => {
+    myParagraph.textContent = "To test the ease of access of uses with disabilities you can use a Google Chrome extension called “Funkify” to simulate other users requirements and then, using this, see if you can improve the website for other people."
+})
+button20.addEventListener('click', () => {
+    myParagraph.textContent = "Hello"
+})
